b6e08939674f7a983c1e98f67dacd95314fdcbf9
carlosgomezr <carlos001909@gmail.com>
2017-03-11 23:07:29 -0600

    Version 6 Script de la Base de Datos Version 3 Script de los Store Procedure Version 2 Script Insert Initial

--- Script_database/[AyD1]Practica2_Store_Procedure.sql
+++ Script_database/[AyD1]Practica2_Store_Procedure.sql
@@ -20,10 +20,12 @@ CREATE PROCEDURE credito
 @descripcion VARCHAR(100)
 AS
 BEGIN
-	INSERT INTO transferencia VALUES (@monto,'',@numero_acreditador, @numero_acreditado,2,@descripcion);
-	UPDATE cuenta SET monto = (SELECT monto from cuenta where numero_cuenta = @numero_acreditador) - @monto WHERE numero_cuenta = @numero_acreditador;
-	UPDATE cuenta SET monto = (SELECT monto from cuenta where numero_cuenta = @numero_acreditado) + @monto WHERE numero_cuenta = @numero_acreditado;
-	 
+	IF(@monto<=(select monto from cuenta where @numero_acreditador = numero_cuenta))
+	BEGIN
+		INSERT INTO transferencia VALUES (@monto,'',@numero_acreditador, @numero_acreditado,2,@descripcion);
+		UPDATE cuenta SET monto = (SELECT monto from cuenta where numero_cuenta = @numero_acreditador) - @monto WHERE numero_cuenta = @numero_acreditador;
+		UPDATE cuenta SET monto = (SELECT monto from cuenta where numero_cuenta = @numero_acreditado) + @monto WHERE numero_cuenta = @numero_acreditado;
+	END 
 END;
 
 --credito 1,4,2000,'Credito a Estuardo de 1000 quetzales';
@@ -37,10 +39,12 @@ CREATE PROCEDURE debito
 @descripcion VARCHAR(100)
 AS
 BEGIN
-	INSERT INTO transferencia VALUES (@monto,'',@numero_debitador, @numero_debitado,3,@descripcion);
-	UPDATE cuenta SET monto = (SELECT monto from cuenta where numero_cuenta = @numero_debitador) + @monto WHERE numero_cuenta = @numero_debitador;
-	UPDATE cuenta SET monto = (SELECT monto from cuenta where numero_cuenta = @numero_debitado) - @monto WHERE numero_cuenta = @numero_debitado;
-	 
+	IF(@monto<=(select monto from cuenta where @numero_debitado = numero_cuenta))
+		BEGIN
+			INSERT INTO transferencia VALUES (@monto,'',@numero_debitador, @numero_debitado,3,@descripcion);
+			UPDATE cuenta SET monto = (SELECT monto from cuenta where numero_cuenta = @numero_debitador) + @monto WHERE numero_cuenta = @numero_debitador;
+			UPDATE cuenta SET monto = (SELECT monto from cuenta where numero_cuenta = @numero_debitado) - @monto WHERE numero_cuenta = @numero_debitado;
+		END	 
 END;
 
 
@@ -54,9 +58,26 @@ CREATE PROCEDURE transferir
 @descripcion VARCHAR(100)
 AS
 BEGIN
-	INSERT INTO transferencia VALUES (@monto,'',@numero, @numero_transferido,1,@descripcion);
-	UPDATE cuenta SET monto = (SELECT monto from cuenta where numero_cuenta = @numero) - @monto WHERE numero_cuenta = @numero;
-	UPDATE cuenta SET monto = (SELECT monto from cuenta where numero_cuenta = @numero_transferido) + @monto WHERE numero_cuenta = @numero_transferido;
-	 
+	IF(@monto<=(select monto from cuenta where @numero = numero_cuenta))
+	BEGIN
+		INSERT INTO transferencia VALUES (@monto,'',@numero, @numero_transferido,1,@descripcion);
+		UPDATE cuenta SET monto = (SELECT monto from cuenta where numero_cuenta = @numero) - @monto WHERE numero_cuenta = @numero;
+		UPDATE cuenta SET monto = (SELECT monto from cuenta where numero_cuenta = @numero_transferido) + @monto WHERE numero_cuenta = @numero_transferido;
+	END 
 END;
 
+
+CREATE PROCEDURE pagoServicio
+@numero_cuenta INTEGER,
+@numero_servicio INTEGER,
+@monto FLOAT,
+@tipo_servicio INTEGER
+AS
+BEGIN
+	IF(@monto<=(select monto from cuenta where @numero_cuenta = numero_cuenta))
+	BEGIN
+		INSERT INTO pago VALUES (@monto,'',@numero_servicio,@numero_cuenta);
+		UPDATE cuenta SET monto = (SELECT monto from cuenta where numero_cuenta = @numero_cuenta) - @monto WHERE numero_cuenta = @numero_cuenta;
+		UPDATE servicio SET monto = (SELECT monto from servicio where numero_cuenta = @numero_servicio) + @monto WHERE numero_cuenta = @numero_servicio;
+	END 
+END;
